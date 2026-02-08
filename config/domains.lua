7ee47f7c282e275b52210a16e1a6c0ebea782633
Amal Thomas <64456630+Amal717@users.noreply.github.com>
2026-02-08 16:15:15 +0530

    Remove Windows SSH and WSL domain settings
    Removed Windows-specific SSH and WSL domain configurations.

--- config/domains.lua
+++ config/domains.lua
@@ -11,33 +11,5 @@ local options = {
    wsl_domains = {},
 }
 
-if platform.is_win then
-   options.ssh_domains = {
-      {
-         name = 'ssh:wsl',
-         remote_address = 'localhost',
-         multiplexing = 'None',
-         default_prog = { 'fish', '-l' },
-         assume_shell = 'Posix',
-      },
-   }
-
-   options.wsl_domains = {
-      {
-         name = 'wsl:ubuntu-fish',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish', '-l' },
-      },
-      {
-         name = 'wsl:ubuntu-bash',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'bash', '-l' },
-      },
-   }
-end
 
 return options
